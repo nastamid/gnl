83e697f4fee231b5fa3982437a02e298e0b91dcf
Nick <nikoloz.astamidze@gmail.com>
2024-12-04 16:24:52 +0100

    added initial method

--- get_next_line.c
+++ get_next_line.c
@@ -10,3 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <get_next_line.h>
+
+#include <stdio.h>
+char *get_next_line(int fd)
+{
+    char k;
+
+    if (fd < 0)
+        return (NULL);
+    
+     while(k = read(fd, BUFFER_SIZE, sizeof(char)))
+     {
+        printf("%c\n", k);
+     }
+
+
+
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -10,3 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#endif
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}					t_list;
+
+char *get_next_line(int fd);
